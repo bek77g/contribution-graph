00dad90de6fff5080a47f1fa29c6be4b34775ed9
bek77g <d.zheenkul@gmail.com>
2023-07-28 17:43:53 +0600

    add: const by mock data

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script>
+  import ContributionGraph from "./components/ContributionGraph.svelte";
+  const contributions = [
+    { date: '2023-06-30', count: 5 },
+    { date: '2023-07-01', count: 5 },
+    { date: '2023-07-02', count: 10 },
+    { date: '2023-07-03', count: 10 },
+    { date: '2023-07-04', count: 7 },
+    { date: '2023-07-05', count: 7 },
+    { date: '2023-07-06', count: 7 },
+    { date: '2023-02-06', count: 7 },
+  ];
 </script>
 
 <main>
+  <ContributionGraph contributions={contributions}/>
 </main>
 
 <style>
